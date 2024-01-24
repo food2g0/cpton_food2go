cfcfebc80c4ef4a14d6a9ff8ce95a915a7b3fb61
food2g0 <foodtogo763@gmail.com>
2024-01-25 00:47:26 +0800

    updated

---
+++ lib/lib/mainScreen/order_details_screen.dart
@@ -0,0 +1,131 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../CustomersWidgets/progress_bar.dart';
+import '../CustomersWidgets/shipment_address_design.dart';
+import '../CustomersWidgets/statusBanner.dart';
+import '../global/global.dart';
+import '../models/address.dart';
+
+class OrderDetailsScreen extends StatefulWidget {
+  final String? orderID;
+
+  OrderDetailsScreen({this.orderID});
+
+  @override
+  _OrderDetailsScreenState createState() => _OrderDetailsScreenState();
+}
+
+class _OrderDetailsScreenState extends State<OrderDetailsScreen> {
+  String orderStatus = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: FutureBuilder<DocumentSnapshot>(
+          future: FirebaseFirestore.instance
+              .collection("users")
+              .doc(sharedPreferences!.getString("uid"))
+              .collection("orders")
+              .doc(widget.orderID)
+              .get(),
+          builder: (context, snapshot) {
+            Map<String, dynamic>? dataMap;
+            if (snapshot.hasData) {
+              dataMap = snapshot.data!.data()! as Map<String, dynamic>;
+              orderStatus = dataMap["status"].toString();
+            }
+            return snapshot.hasData
+                ? Container(
+
+              child: Column(
+                children: [
+                  StatusBanner(
+                    status: dataMap!["isSuccess"],
+                    orderStatus: orderStatus,
+                  ),
+                  const SizedBox(height: 10.0),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "Total Amount Php " + dataMap["totalAmount"].toString(),
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: "Poppins",
+                        ),
+                      ),
+                    ),
+                  ),
+                   Padding(
+                     padding: const EdgeInsets.all(8.0),
+                     child: Align(
+                       alignment: Alignment.centerLeft,
+                       child: Text(
+                          "Order Id = " + widget.orderID!,
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontFamily: "Poppins",
+                          ),
+                        ),
+                     ),
+                   ),
+
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                          "Order at: " +
+                              DateFormat("dd MMMM, yyyy - hh:mm aa").format(
+                                DateTime.fromMillisecondsSinceEpoch(
+                                  int.parse(dataMap["orderTime"]),
+                                ),
+                              ),
+                          style: const TextStyle(
+                            fontSize: 16,
+                            color: Colors.grey,
+                            fontFamily: "Poppins",
+                          ),
+                        ),
+                    ),
+                  ),
+
+                  const Divider(thickness: 4),
+                  orderStatus == "ended"
+                      ? Image.asset("images/delivered.jpg")
+                      : Image.asset("images/state.jpg"),
+                  const Divider(thickness: 4),
+                  FutureBuilder<DocumentSnapshot>(
+                    future: FirebaseFirestore.instance
+                        .collection("users")
+                        .doc(sharedPreferences!.getString("uid"))
+                        .collection("userAddress")
+                        .doc(dataMap["addressID"])
+                        .get(),
+                    builder: (context, snapshot) {
+                      return snapshot.hasData
+                          ? ShipmentAddressDesign(
+                        model: Address.fromJson(
+                          snapshot.data!.data()!
+                          as Map<String, dynamic>,
+                        ),
+                      )
+                          : Center(child: circularProgress());
+                    },
+                  ),
+                ],
+              ),
+            )
+                : Center(child: circularProgress());
+          },
+        ),
+      ),
+    );
+  }
+}
