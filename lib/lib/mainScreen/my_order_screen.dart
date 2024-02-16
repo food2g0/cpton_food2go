2eb39d539c1227aaf604edaca1e5c5f5fbdf6540
food2g0 <foodtogo763@gmail.com>
2024-02-17 00:16:39 +0800

    yft

--- lib/lib/mainScreen/my_order_screen.dart
+++ lib/lib/mainScreen/my_order_screen.dart
@@ -185,7 +185,7 @@ class _MyOrderScreenState extends State<MyOrderScreen> {
             .collection("users")
             .doc(sharedPreferences!.getString("uid"))
             .collection("orders")
-            .where("status", isEqualTo: "normal")
+            .where("status", isEqualTo: "accepted")
             .snapshots(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
